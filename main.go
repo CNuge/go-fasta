c571a5ff86650d36a783cfdb0ad29f456ea8d929
WATTBA <nugentc@uoguelph.ca>
2018-03-24 16:01:29 -0400

    flag docs

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ import(
 
 
 // include arguments for:
-	// -s summary:
-		// if passed then produce a summary file
-
-	// -n ncbi (batch or single)
-		// take either one string, multiple space delimited string or a text file
-		// parse the above into a slice of accession numbers, query NCBI for the accession numbers
-	
 
 	// -m merge (multiple, output file name)
 		// take a list of files space delimited (or a .txt with filenames within)
@@ -34,6 +27,25 @@ import(
 			// take the large Fasta struct and Write it to -f
 
 
+	// -n ncbi (batch or single)
+		// take either one string, multiple space delimited string or a text file
+		// parse the above into a slice of accession numbers, query NCBI for the accession numbers
+
+
+	// -f if passed, change the output file names 
+		// for instance of both a .fasta and a summary, 
+		// take this name and split on a . , take the first bit and append .fasta and .txt to it and use accordingly
+	
+
+	// -s summary:
+		// if passed then produce a summary file
+
+
+	// -a alphabetize the sequences in a fasta by name
+		// read in the fasta
+		// call the sort function on the fasta
+		// Write() to the input name
+
 	// -split (single, output file names == fasta names)
 		// for the split, have it take a list fasta struct, and split each member of 
 		// the fasta into its own fasta struct, then take this and write each to a file
@@ -45,16 +57,6 @@ import(
 			// for each seq in the Fasta struct, make a new Fasta struct and
 			// call Write() pass in the seq.name + ".fasta" as the output name for each 
 
-	// -f if passed, change the output file names 
-		// for instance of both a .fasta and a summary, 
-		// take this name and split on a . , take the first bit and append .fasta and .txt to it and use accordingly
-	
-
-	// -a alphabetize the sequences in a fasta by name
-		// read in the fasta
-		// call the sort function on the fasta
-		// Write() to the input name
-
 func main(){
 
 	wordPtr := flag.String("word", "foo", "a string")
@@ -65,10 +67,6 @@ func main(){
 	// this is a fork boolean pointer, if not passed, it is false, whe -fork is passed it becomes true
 
 
-	filePtr := flag.String("f", "output.fasta", "A .fasta or .txt filename.\n" + 
-												"For use with -m -n and -s flags to speficy an output name.\n" +
-												"If both a fasta and summary are needed, just passed a .fasta name\n" + 
-												"and it will produce a summary file with the same name and a .txt extension\n")
 
 	mergePtr := flag.String("m", nil, "A comma delimited list of fasta filenames to be merged.\n" +
 										"The final fasta will contain the sequences in the order of the .fasta inputs.\n" +
@@ -80,10 +78,18 @@ func main(){
 									"You an also pass in a .txt filename which contains a list of IDs (all specified on seprate lines).\n" +
 									"Use in conjunction with the -f flag to alter the output file name.")
 
-	splitPtr := flag.Bool("split", false, "Pass this flag in to split the output fasta into a separate fasta file\n" +
-											"for each of its component sequences.\n")
+	filePtr := flag.String("f", "output.fasta", "A .fasta or .txt filename.\n" + 
+												"For use with -m -n and -s flags to speficy an output name.\n" +
+												"If both a fasta and summary are needed, just passed a .fasta name\n" + 
+												"and it will produce a summary file with the same name and a .txt extension\n")
 
-	alphaPtr := flag.String("a", "", "The sequences in the specified fasta file will be alphabetized by sequence name.\n")
+	alphaPtr := flag.Bool("a", false, "Pass this flag name in conjunction with a -f flag.\n" +
+										"Sequences in the -f specified file will be sorted alphabetically by sequence name.\n")
+
+
+	splitPtr := flag.Bool("split", false, "Pass this flag name in conjunction with a -f flag.\n" +
+											"The Sequences in the -f specified file will be split into a set of fasta files, one for each sequence in the file.\n")
+
 
 
 	// parse the command line arguments
